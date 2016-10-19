fe15b9af07e5cd684f54d0ca922c9437391fdeca
R <R@RPING>
2016-10-19 14:14:05 -0400

    Add project files.

---
+++ CunaLoadManagementSystemViews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CunaLoadManagementSystemViews.Startup))]
+namespace CunaLoadManagementSystemViews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
